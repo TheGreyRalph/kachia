7716823245492eead42e75e11595b6ed4fc79345
redman-tp <withneywayne@gmail.com>
2024-12-14 11:38:58 +0100

    Little drawar mod

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -71,6 +71,29 @@
         class="q-hidden-md-and-up cus-drawer-md"
       >
         <div class="cus-drawer-cont-md">
+          <button class="cus-nav-close-btn" @click="toggleLeftDrawer">
+            <svg
+              fill=""
+              width="64px"
+              height="64px"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g
+                id="SVGRepo_tracerCarrier"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ></g>
+              <g id="SVGRepo_iconCarrier">
+                <g>
+                  <path
+                    d="M10,1a9,9,0,1,0,9,9A9,9,0,0,0,10,1Zm0,16.4A7.4,7.4,0,1,1,17.4,10,7.41,7.41,0,0,1,10,17.4ZM13.29,5.29,10,8.59,6.71,5.29,5.29,6.71,8.59,10l-3.3,3.29,1.42,1.42L10,11.41l3.29,3.3,1.42-1.42L11.41,10l3.3-3.29Z"
+                  ></path>
+                </g>
+              </g>
+            </svg>
+          </button>
           <div class="cus-header-logo-cont">
             <img src="images/klogo-nbg.png" alt="Kachia Culture" class="" />
             <h2 class="">Kachia Local Government Area</h2>
